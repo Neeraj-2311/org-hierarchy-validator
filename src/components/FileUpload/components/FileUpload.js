8679036a8828a260b7b580361253ea9a44ff7584
Neeraj <113253776+Neeraj-2311@users.noreply.github.com>
2025-01-15 17:00:03 +0530

    Update FileUpload.js

--- src/components/FileUpload/components/FileUpload.js
+++ src/components/FileUpload/components/FileUpload.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import UploadError from './UploadError';
 
 const FileUpload = ({ onFileUpload, errorMessage, file, setFile }) => {
